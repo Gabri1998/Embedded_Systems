de080913db84a8a61e84a759744050f4e72156b7
David Hong <davidbor@net.chalmers.se>
2024-01-30 15:27:00 +0100

    Add new comments

--- WP1/exercise4Bin2Hex.c
+++ WP1/exercise4Bin2Hex.c
@@ -13,6 +13,7 @@
 int SetBit(int OldValue, int BitNum)
 {
   int newValue = (OldValue | (1 << BitNum)); // compare the value with the shifted bit to the  left using |
+  // Return the value
   return newValue;
 }
 
@@ -20,11 +21,14 @@ int SetBit(int OldValue, int BitNum)
 int ResetBit(int OldValue, int BitNum)
 {
   int newValue = (OldValue & (~(1 << BitNum))); // compare the value with the shifted bit to the  left  using &/~
+  // Return the value
   return newValue;
 }
 
+// A function to change the String Binary to Hexadecimal
 void StrBinToHex(char *str, char *value)
 {
+  // Declare the variables
   short FourBit = 0;  // number var to store 4 bit to Set and Reset
   int FourBitCnt = 0; // indicator for 4 bit
   int ArrIdx = 0;     // array index
@@ -40,13 +44,17 @@ void StrBinToHex(char *str, char *value)
       // Reset bit number in var FourBit
       FourBit = ResetBit(FourBit, FourBitCnt);
     }
+
+    // If the str[idxSZ] is equal to 1
     else if (str[idxSZ] == '1')
     {
       // Set bit number in var FourBit
       FourBit = SetBit(FourBit, FourBitCnt);
     }
+    // Other case
     else
     {
+      // Break out of the program
       break;
     }
 
@@ -56,16 +64,19 @@ void StrBinToHex(char *str, char *value)
     // if caught 4 bits, handle them as hex value
     if (FourBitCnt == 4)
     {
+      // Case where the fourbit is between 0 and 9
       if (FourBit >= 0 && FourBit <= 9)
       {                                  // check if the 4bits between 0 and 9
         value[ArrIdx++] = FourBit + '0'; // storing the bits to the array then incerment
       }
+      // Case where fourbut is larger than 10
       else if (FourBit >= 10)
       {                                       // check if the 4bits is between the 10 and 15
         value[ArrIdx++] = FourBit - 10 + 'A'; // storing the bits to the array then incerment
       }
-
+      // Change the fourbit to 0
       FourBit = 0;
+      // Change the fourbitcnt to 0
       FourBitCnt = 0;
     }
   }
@@ -73,10 +84,12 @@ void StrBinToHex(char *str, char *value)
   // If there are remaining bits, handle them
   if (FourBitCnt > 0)
   {
+    // Case where fourbit is between 0 and 9
     if (FourBit >= 0 && FourBit <= 9)
     {                                  // check if the 4bits between 0 and 9
       value[ArrIdx++] = FourBit + '0'; // storing the bits to the array then incerment
     }
+    // Case where fourbit is larger than 10
     else if (FourBit >= 10)
     {                                       // check if the 4bits is between the 10 and 15
       value[ArrIdx++] = FourBit - 10 + 'A'; // storing the bits to the array then incerment
@@ -85,40 +98,50 @@ void StrBinToHex(char *str, char *value)
   // Reverse the hex string in-place
   for (int start = 0, end = ArrIdx - 1; start < end; start++, end--)
   {
-    char temp = value[start];
+    char temp = value[start];  // set the temporary char and store the start value
     value[start] = value[end]; // the swaping happens here
-    value[end] = temp;
+    value[end] = temp;         // set the temp value to the value[end]
   }
   // Add null terminator at the end of the hex string
   value[ArrIdx] = '\0';
 }
 
+// Start of the main function
 int main(int argc, char *argv[])
 {
-  unsigned int x;
+  // Declare the variables
+  unsigned int x; // Declare a variable x
 
   char output[INDEX] = {0}; // store the output
 
+  // If the argument is less or equal to 1
   if (argc <= 1)
   {
     printf("No argument provided for more information use \'-h\'\n");
+    // Return 2 to exit with failure
     return 2;
     // check if the user pass more than one argument
   }
+  // If the argument is not 2
   else if (argc != 2)
   {
     printf("Only one Argument expected for more information use \'-h\' \n");
 
+    // Return with 2 to exit the program
     return 2;
   }
+
+  // Other cases
   else
   {
     // Using external library to check if the user pass -h for help
 
+    // If the first argument is equal to -h
     if (strcmp(argv[1], "-h") == 0)
     {
       printf("you should pass the name of the file then the first argment should be a digit\n");
     }
+    // Other cases
     else
     {
 
--- WP1/exercise4Dec2Bin.c
+++ WP1/exercise4Dec2Bin.c
@@ -4,6 +4,7 @@
 // Exercise 4a
 // Submission code: XXXXXX (provided by your TA-s)
 
+// Include section for the header files
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,7 +13,9 @@
 // set the bits
 int SetBit(int OldValue, int BitNum)
 {
+
   int newValue = (OldValue | (1 << BitNum)); // compare the value with the shifted bit to the  left using |
+  // Return the new value
   return newValue;
 }
 
@@ -20,24 +23,30 @@ int SetBit(int OldValue, int BitNum)
 int ResetBit(int OldValue, int BitNum)
 {
   int newValue = (OldValue & (~(1 << BitNum))); // compare the value with the shifted bit to the  left  using &/~
+  // Return the new value
   return newValue;
 }
 
+// Declare a new function to change the string decimal into decimal
 void StrDecToDec(char *str, unsigned int *value)
 {
   // 12345 str = >> int 12345 - atoi
   // strlen(str),
   char *strCpy = str; // copying the arument
   int num = 0;        // setting  a variable to store the output
+  // While the copy of the string is not a null
   while (strCpy != NULL)
   { // loop while the string is not null
+    // See if the string copy is between 0 and 9
     if (*strCpy >= '0' && *strCpy <= '9')
     {                                       // check the tstring is a digits between 0 and 9
       num = ((num * 10) + (*strCpy - '0')); // 123 converting to int
     }
+    // Other case
     else
     {
 
+      // Exit the loop with a break
       break;
     }
     strCpy++; // incerment the string to the next charecheter
@@ -45,9 +54,11 @@ void StrDecToDec(char *str, unsigned int *value)
   *value = num; // setting the attribute value to num
 }
 
+// Declare a new function to exchange the decimal to a binary
 void DecToBin(unsigned int var, char *value)
 {
 
+  // Declare the variables
   int ArrIdx = 0;        // array index
   int sizeValue = 31;    // intial size of the bits
   int bb = 0;            // store the var after the shifting
@@ -68,6 +79,7 @@ void DecToBin(unsigned int var, char *value)
       value[ArrIdx] = '1'; // store the ones in an array
       ArrIdx++;            // incerment the indexes
     }
+    // Other cases
     else
     { // print zero's
       printf("0");
@@ -79,28 +91,33 @@ void DecToBin(unsigned int var, char *value)
   value[ArrIdx] = '\0'; // Add null terminator at the end of the hex string
 }
 
+// Beginning of the main function
 int main(int argc, char *argv[])
 {
+  // Declare variables
   unsigned int x;
 
+  // Set the first element as a 0
   char output[INDEX] = {0};
 
+  // If the argument is less or equal to 1
   if (argc <= 1)
   {
     printf("No argument provided for more information use \'-h\'\n");
+    // Return 2 to exit with failure
     return 2;
     // check if the user pass more than one argument
   }
+  // If the argument is not 2
   else if (argc != 2)
   {
     printf("Only one Argument expected for more information use \'-h\' \n");
-
+    // Return 2 to exit with failure
     return 2;
   }
   else
   {
     // Using external library to check if the user pass -h for help
-
     if (strcmp(argv[1], "-h") == 0)
     {
       printf("you should pass the name of the file then the first argment should be a digit\n");
