c0aac45d6d6490022ca6878bd2a2f8ca747e403d
amandaturquis <amandaturquis@live.com>
2024-02-05 16:53:34 +0100

    Exercise 2

--- WP2/exercise2.c
+++ WP2/exercise2.c
@@ -8,6 +8,7 @@
 #include <time.h>
 // #### Constants #####
 #define MAX 5
+
 // ##### typedefs ####
 typedef struct q
 {
@@ -15,9 +16,12 @@ typedef struct q
     struct q *next;
     struct q *prev;
 } REGTYPE;
+
 // ##### Function declarations #####
 REGTYPE *random_list(void);
 REGTYPE *add_first(REGTYPE *temp, int data);
+
+
 // ###### Main program #######
 int main(int argc, char *argv[])
 {
@@ -26,10 +30,12 @@ int main(int argc, char *argv[])
     srand(time(0)); // Random seed
     head = random_list();
     act_post = head;
+
     while (act_post != NULL)
     {
         printf("\n Post nr %d : %d", nr++, act_post->number);
         act_post = act_post->next;
+        printf("\n\n");
     }
 
     // --- Free the allocated memory ---
@@ -38,16 +44,83 @@ int main(int argc, char *argv[])
         head = act_post->next;
         free(act_post);
     }
+    
+
+    int newNode = 40;
+    head = add_first(head, newNode); // call the function to add a new record at the beginning
+
+    // Print new list
+    nr = 0; // reset the post number
+    act_post = head;
+    while (act_post!= NULL) // while the head is not null, it will print post number and its data
+    {
+        printf("\n Post nr %d : %d", nr++, act_post->number);
+        act_post = act_post->next; // move to the next node in the list
+        printf("\n\n");
+    }
+    
     return 0;
+
 }
 // ==== End of main ======================================
+
+
 REGTYPE *random_list(void)
 {
     int nr, i = 0;
     REGTYPE *top, *old, *item;
-    return (top);
+
+    for (i = 0; i < MAX; i++)
+    {
+        item = (REGTYPE*)malloc(sizeof(REGTYPE)); // allocate memory for a new node
+        if (item == NULL) // Check memmory allocation and prints error message
+        {
+            printf("Failed memory allocation.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        item -> number = rand()%101; // generates a random number between 0 and 100
+        item -> next = NULL; // initialize the next pointer to null
+
+        if (old != NULL) // if old is not null it set to the next item
+        {
+            old -> next = item; // set next of the previous node to the current one
+            item -> prev = old; // set prev of the current node to the previous one
+        }
+        else
+        {
+            top = item; // if is the first node, set top to the current
+            item -> prev = NULL; // set prev to null for the first node
+        }
+        
+        old = item; // move the older pointer to the current node
+        
+    }
+    
+
+    return (top); //return pointer to the head of the list
 }
 //==========================================================
+
 REGTYPE *add_first(REGTYPE *temp, int data)
 {
+    REGTYPE *newItem = (REGTYPE*)malloc(sizeof(REGTYPE)); // allocate memory for a new node
+
+    if (newItem == NULL) // checks if memory allocation and prints error message
+    {
+       printf("Memory allocation failed.\n");
+       exit(EXIT_FAILURE);
+    }
+
+    newItem -> number = data; // set number field of the newItem to data
+    newItem -> next = temp; // set next field of the newItem to point to the current head of the list
+    newItem -> prev = NULL; // set prev field of the newItem to null
+
+    if (temp != NULL)
+    {
+        temp -> prev = newItem; // update prev field of the current head if exists
+    }
+    
+    return newItem; // return the pointer to the new head of the list
+    
 }
