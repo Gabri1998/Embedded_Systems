1773b6d81ad8b5f61704a8885020147931f746ef
amandaturquis <amandaturquis@live.com>
2024-02-06 22:01:37 +0100

    exercise 1

--- WP3/exercise1.c
+++ WP3/exercise1.c
@@ -3,27 +3,26 @@
 // Exercise 1
 // Submission code: XXXXXX (provided by your TA-s)
 
-LED_BUILTIN;
 OUTPUT;
 HIGH;
 LOW;
 INPUT;
 
 // C++ code
-//
+
 int pushbutton = 0;
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  //pinMode(13, OUTPUT);
   pinMode(12, INPUT);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(13, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(13, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 
   // read state pushbutton
@@ -31,9 +30,9 @@ void loop()
   // check if pushbutton is pressed
   if (pushbutton < HIGH) {
     // turn LED on
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(12, HIGH);
   } else {
     // turn LED off
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(12, LOW);
   }
 }
