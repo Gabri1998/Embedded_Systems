6206bc859e38b32a1d1de6b93168d6279a5bbaba
Gabri <Gabri@student.chalmers.se>
2024-02-06 13:45:48 +0100

    WP2 done

---
+++ WP2/code.c
@@ -0,0 +1,109 @@
+// (C) Amanda Turquis,Ahmed Algabri,David Hong, group: 13 (2024)
+// Work package 2
+// Exercise 4a
+// Submission code: XXXXXX (provided by your TA-s)
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void checkValue(char val, char start, char end) {
+  // we just need to change it to return int if the exit(0); considered a bad practice
+  //   int status = 0;
+  //   if (val >= start && val <= end) {
+  //     // correct
+  //     // packingvalue(val, argidx);
+  //     status = 0;
+  //   } else {
+  //     printf("you entered invalid value\n");
+  //     exit(0);
+  //     status = 1;
+  //     // fail
+  //   }
+
+  if (!(val >= start && val <= end)) { // chck if the value is not  in the range 
+    printf("you entered invalid value, range: <0..1> <0..4> <0..2> <0..1> <0..1>\n");
+    exit(0);
+  }
+
+  //   return status;
+}
+
+typedef struct { // struct to  pack the variables
+  unsigned char engine_on : 1;
+  unsigned char gear_pos : 3;
+  unsigned char key_pos : 2;
+  unsigned char brake_F : 1;
+  unsigned char brake_B : 1;
+} car;
+
+int main(int argc, char *argv[]) {
+
+    
+       
+    // int checkStat = 0;
+        
+      if(argc != 6){  // check the number of arguments
+     printf("Usage: %s <engine_on> <gear_pos> <key_pos> <brake1> <brake2>\n", argv[0]);
+     return 1;
+    }else{
+         car c ; // a struct of car type
+     unsigned char res = 0; // char to store the values of bits
+     int cnt = 1; // to count the the arguments 
+     char range[6][2] = {{'0', '0'}, {'0', '1'}, {'0', '4'},
+                         {'0', '2'}, {'0', '1'}, {'0', '1'}}; // defining the range for each argument respectfly in order
+     while (cnt < 6) {
+       if (strlen(argv[cnt]) == 1) { // count from one to 5
+         checkValue(argv[cnt][0], range[cnt][0], range[cnt][1]); // call the method to check
+       } else {
+         printf("you entered more than 1 digit in a single arument<>\n");
+         exit(0);
+       }
+       cnt++; // incerment
+     }
+
+     c.engine_on = atoi(argv[1]);  //convering the input to digits 
+     c.gear_pos = atoi(argv[2]);
+     c.key_pos = atoi(argv[3]);
+     c.brake_F = atoi(argv[4]);
+     c.brake_B = atoi(argv[5]);
+
+    printf("size of c:%d", sizeof(c));
+   
+     printf("\n%-15s%-10s\n", "Name", "Value");
+    printf("-----------------------\n");
+    printf("%-15s%d\n", "size c.engine_on:", c.engine_on);
+    printf("%-15s%d\n", "size c.gear_pos :", c.gear_pos);
+    printf("%-15s%d\n", "size c.key_pos  :", c.key_pos);
+    printf("%-15s%d\n", "size c.brake_F  :", c.brake_F);
+    printf("%-15s%d\n\n", "size c.brake_B  :", c.brake_B);
+
+     res |= (c.engine_on & 0b00000001) << 7; // packing by chefting to left by 7 and using & 
+      //  printf("res:%x\n", res);
+    res |= (c.gear_pos & 0b00000111) << 4; // packing by chefting to left by 4 and using & 
+    //   printf("res:%x\n", res);
+     res |= (c.key_pos & 0b00000011) << 2; // packing by chefting to left by 2 and using & 
+     //   printf("res:%x\n", res);
+     res |= (c.brake_F & 0b00000001) << 1; // packing by chefting to left by 1 and using & 
+     //   printf("res:%x\n", res);
+     res |= (c.brake_B & 0b00000001);
+     printf("result:%x\n", res);
+
+     printf("size of c:%x\n", c);
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    return 0;}
+  
---
+++ WP2/decode.c
@@ -0,0 +1,91 @@
+// (C) Amanda Turquis,Ahmed Algabri,David Hong, group: 13 (2024)
+// Work package 2
+// Exercise 4b
+// Submission code: XXXXXX (provided by your TA-s)
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// Check the value in hex using Ascii table
+void checkDecode(char *value) {
+ if (value[0] >= 'A' && value[0] <= 'Z'){ // Check the first digit og the input
+value[0]+=32;  // change it to  small 
+ }
+ if (value[1] >= 'A' && value[1] <= 'Z'){//Check the seconed digit of the input
+value[1]+=32;   // change it to  small
+ }
+  if (value[0] > 'c' || value[1] > 'b')  { //check if the value is between C and B in hexa
+    printf("you entered invalid value max cb\n");
+    exit(0); // exit with code 0
+  }
+
+  //   return status;
+}
+
+typedef struct {  // a struct to store the and pack the variables
+  unsigned char engine_on : 1;
+  unsigned char gear_pos : 3;
+  unsigned char key_pos : 2;
+  unsigned char brake_F : 1;
+  unsigned char brake_B : 1;
+} car;
+
+int main(int argc, char *argv[]) {
+    
+
+
+    if(argc == 2){ // check the number of arguments
+
+      if (strlen(argv[1]) != 2) { // check the number of digit provided in the argument
+   
+   printf("Invalid input, max 2 digit allowed in hexa\n");
+   
+      } else {  // calling the method to check the input
+        checkDecode(argv[1]);
+
+        unsigned char tmp = 0; // char to store the values of bits
+      car c ; // a struct of car type
+      if (argv[1][0] >= '0' && argv[1][0] <= '9') { //check if the first digit of value between 0 and 9
+       tmp = argv[1][0] - '0';
+      } else if (argv[1][0] >= 'a' && argv[1][0] <= 'f') { //check if the first digit of value between a and f
+       tmp = (argv[1][0] - 'a') + 10;
+      } else if (argv[1][0] >= 'A' && argv[1][0] <= 'F') { //check if the first digit of value between A and F
+      tmp = (argv[1][0] - 'A') + 10;
+       }
+       //  tmp =>  0b0000 1111
+       tmp <<= 4; // shift to the left 
+      //  tmp =>  0b1111 0000
+      if (argv[1][1] >= '0' && argv[1][1] <= '9') { //check if the seconed digit of value between 0 and 9
+       tmp |= argv[1][1] - '0';
+       } else if (argv[1][1] >= 'a' && argv[1][1] <= 'f') { //check if the seconed digit of value between a and f
+      tmp |= (argv[1][1] - 'a') + 10;
+      } else if (argv[1][1] >= 'A' && argv[1][1] <= 'F') { //check if the seconed digit of value between A and F
+      tmp |= (argv[1][1] - 'A') + 10;
+       }
+
+     c.engine_on = tmp >> 7; // unpacking by chefting to right by 7
+     c.gear_pos = tmp >> 4; // unpacking by chefting to right by 4
+     c.key_pos = tmp >> 2;  // unpacking by chefting to right by 2
+     c.brake_F = tmp >> 1;  // unpacking by chefting to right by 1
+     c.brake_B = tmp; // last bit
+     printf("\n%-15s%-10s\n", "Name", "Value");
+    printf("-----------------------\n");
+    printf("%-15s%d\n", "engine_on", c.engine_on);
+    printf("%-15s%d\n", "gear_pos", c.gear_pos);
+    printf("%-15s%d\n", "key_pos", c.key_pos);
+    printf("%-15s%d\n", "brake_F", c.brake_F);
+    printf("%-15s%d\n\n", "brake_B", c.brake_B);
+
+     }
+     
+      
+    }else{
+     printf("Only one argument expected  code.exe <hexa>\n");
+      exit(1);
+ 
+    }
+    
+    
+    
+    return 0;}
